1a6fc164c62df8f51d1b4dc382d29838049f8f5c
Daylight . <naoldana5@gmail.com>
2024-12-12 20:56:22 +0300

    implemented push and pop methods for stuck using linked list

--- different dsa implementations/stuck.cpp
+++ different dsa implementations/stuck.cpp
@@ -1,6 +1,52 @@
 #include <iostream>
 using namespace std;
-int main ()
+struct stuck
 {
-    cout<<"hello world"<<endl;
+    int data;
+    stuck* next;
+};
+stuck* top=nullptr;
+void push(int data);
+void display(stuck* top);
+int pop();
+int main ()
+{  stuck* first= new stuck;
+    first->data=1;
+    first->next=nullptr;
+    top=nullptr;
+    
+    display(top);
+    int x= pop();
+    cout<<x<<endl;
+    display(top);
+    
+    
+}
+void push(int data)
+{
+    stuck* temp= new stuck;
+    temp->data=data;
+    temp->next=top;
+    top=temp;
+    delete temp;
+}
+
+int pop()
+{  if(top==nullptr)
+    {  cout<<"stuck underflow"<<endl;
+       return -1;
+    }
+   else 
+     {  stuck* temp=top;
+         top=top->next;
+         return temp->data;
+     }
+
+}
+void display(stuck* top)
+{  while ( top!=nullptr)
+   { cout<<top->data<<" ";
+     top=top->next;
+    }
+
 }
