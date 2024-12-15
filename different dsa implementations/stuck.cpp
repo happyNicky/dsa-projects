6f5667b31a7f2b4b43a15df82ec704291127c08d
Daylight . <naoldana5@gmail.com>
2024-12-15 10:21:23 +0300

    function for reversing linkedlist using stuck

--- different dsa implementations/stuck.cpp
+++ different dsa implementations/stuck.cpp
@@ -1,52 +1,106 @@
 #include <iostream>
+#include <stack>
 using namespace std;
+
+
 struct stuck
 {
     int data;
     stuck* next;
 };
-stuck* top=nullptr;
+stuck* top = nullptr;
 void push(int data);
 void display(stuck* top);
 int pop();
+bool isEmpty(stuck* top);
+int topElement(stuck* top);
+string reverseString(string s);
+void reverseLinkedListUsingStack(stuck* &top);
 int main ()
-{  stuck* first= new stuck;
-    first->data=1;
-    first->next=nullptr;
-    top=nullptr;
-    
-    display(top);
-    int x= pop();
-    cout<<x<<endl;
+{
+    push(1);
+    push(2);
+    push(3);
+    push(4);
     display(top);
     
-    
+    reverseLinkedListUsingStack(top);
+    pop();
+    cout<<endl;
+    display(top);
 }
+
 void push(int data)
 {
-    stuck* temp= new stuck;
-    temp->data=data;
-    temp->next=top;
-    top=temp;
-    delete temp;
+    stuck* temp = new stuck;
+    temp->data = data;
+    temp->next = top;
+    top = temp;
 }
-
 int pop()
-{  if(top==nullptr)
-    {  cout<<"stuck underflow"<<endl;
-       return -1;
+{
+    if(top == nullptr)
+    {
+        cout << "stuck underflow" << endl;
+        return -1;
     }
-   else 
-     {  stuck* temp=top;
-         top=top->next;
-         return temp->data;
-     }
-
+    else
+    {
+        stuck* temp = top;
+        top = top->next;
+        int data = temp->data;
+        delete temp;
+        return data;
+    }
 }
 void display(stuck* top)
-{  while ( top!=nullptr)
-   { cout<<top->data<<" ";
-     top=top->next;
+{
+    while (top != nullptr)
+    {
+        cout << top->data << " ";
+        top = top->next;
     }
-
 }
+bool isEmpty(stuck* top)
+{
+    return top == nullptr;
+}
+int topElement(stuck* top)
+{
+    return top->data;
+}
+
+string reverseString(string s)
+{
+    stack<char> sta;
+    for(char c : s)
+        sta.push(c);
+    
+    for(size_t i = 0; i < s.length(); i++)
+    {
+        s[i] = sta.top();
+        sta.pop();
+    }
+    return s;
+}
+
+void reverseLinkedListUsingStack(stuck* &top)
+ { 
+    stuck* temp=top; 
+    stack<stuck*> s;
+    while(temp!=nullptr) 
+    { s.push(temp);
+     temp=temp->next; 
+    } 
+    temp=s.top();
+    
+     top = temp;
+      s.pop();
+       while(!s.empty()) {
+         temp->next=s.top(); 
+         s.pop(); 
+         temp=temp->next; 
+         } 
+         temp->next=nullptr;
+}
+
