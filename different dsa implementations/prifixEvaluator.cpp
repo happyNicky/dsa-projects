1c0a76803b056494b1630903b3169a0f08895bd8
Daylight . <naoldana5@gmail.com>
2024-12-22 16:44:22 +0300

    prifix evaluation using c++

--- different dsa implementations/prifixEvaluator.cpp
+++ different dsa implementations/prifixEvaluator.cpp
@@ -5,26 +5,46 @@
 #include<algorithm>
 
 using namespace std;
-int prifixEval(string expression);
-int eval(int x,int y,char op);
+float prifixEval(string expression);
+float eval(float x,float y,char op);
 int main()
-{
+{ string k="- + * 2 3 * 5 4 9";
+  cout<<prifixEval(k);
 
 }
-int prifixEval(string expression)
-{   stack<int> k;
-    int x,y;
+float prifixEval(string expression)
+{   stack<float> k;
+    float x,y;
     reverse(expression.begin(),expression.end());
     stringstream ss(expression);
     string token;
-    while(ss>>token)
+    while(ss >> token)
     {     if (token == "*" || token == "+" || token == "-" || token == "/" || token == "^") {
-                x=
-                            
+                x= k.top(); k.pop();
+                y= k.top(); k.pop();
+              k.push( eval(x,y,token[0]));             
          }
-
-
+         else 
+         {   
+            k.push(stoi(token));
+         }
     }
+    return k.top();
 
 }
-int eval(int x,int y,char op);
+
+float eval(float x, float y, char op) {
+    switch (op) {
+        case '*':
+            return x * y;
+        case '+':
+            return x + y;
+        case '-':
+            return x - y;
+        case '/':
+            return x/ y;
+        case '^':
+            return pow(x, y);
+    }
+    return 0;
+}
